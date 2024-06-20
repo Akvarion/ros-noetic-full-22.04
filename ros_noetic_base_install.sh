1f6da55f3c8ae3b8bd83a1c3dcd7ee255539caea
Akvarion <newplayerone@linuxmail.org>
2024-06-20 17:08:24 +0200

    update

--- ros_noetic_base_install.sh
+++ ros_noetic_base_install.sh
@@ -33,11 +33,11 @@ git clone https://github.com/ros/roslisp.git -b 1.9.25
 git clone https://github.com/ros/rospack.git -b 2.6.2
 git clone https://github.com/ros/std_msgs.git -b 0.5.13
 
-cd pluginlib && git apply --ignore-whitespace ../../../patches/pluginlib.patch
+cd pluginlib && git apply --ignore-whitespa/ce ../../../patches/pluginlib.patch
 cd ../ros_comm && git apply --ignore-whitespace ../../../patches/ros_comm.patch
 cd ../rosconsole && git apply --ignore-whitespace ../../../patches/rosconsole.patch
 
-cd ../..
+cd ../../..
 git clone https://github.com/ros-infrastructure/catkin_pkg.git -b 0.5.2
 git clone https://github.com/ros-infrastructure/rospkg.git -b 1.5.0
 cd catkin_pkg && python3 setup.py install
